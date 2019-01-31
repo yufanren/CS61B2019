e004a7e7f3c8484aa79fe61b2a42a1d6ff5c7eea
yufan <yufan@yufans-MacBook-Air.local>
2019-01-30 22:20:13 -0500

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 0;
+        while (x < 10) {
+        	y = y + x;
+            System.out.print(y + " \n");
+            x = x + 1;
+        }
+    }
+}
