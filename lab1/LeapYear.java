905b143c6461290e946d0d241a7cb4b62e9a0ba5
yufan <yufan@yufans-MacBook-Air.local>
2019-01-31 05:18:58 -0500

    Completed LeapYear.

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,16 @@ public class LeapYear {
             }
         }
     }
+    /** Check if year is leap year */ 
+    public static boolean isLeapYear(int year){
+            if (year % 400 == 0){
+                return true;
+            } else if (year % 4 == 0){
+                if (year % 100 != 0){
+                    return true;
+                }
+            }
+            return false;
+    }
 }
 
