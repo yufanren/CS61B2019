44c4ff717c6bc225e74c4753c23bc0d0d78f6880
yufan <yufan@yufans-MacBook-Air.local>
2019-02-03 22:16:13 -0500

    Set G constant to private.

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,6 +1,6 @@
 public class Planet {
 	/** Class Variables. */
-	public static final double G = 6.67e-11;
+	private static final double G = 6.67e-11;
 	public double xxPos;
 	public double yyPos;
 	public double xxVel;
