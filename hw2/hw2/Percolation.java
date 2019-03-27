f4366cb5941099afd130407d4a7b8b634666ffdc
Yufan Ren <yufanren@yahoo.com>
2019-03-27 11:27:15 -0400

    HW2 complete.

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,97 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private int pLength;
+    private boolean[][] pTron;
+    private WeightedQuickUnionUF pList1;
+    private WeightedQuickUnionUF pList2;
+    private int pTop;
+    private int pBottom;
+    private int openNodes;
+
+    /* Create a Percolation object with length and height N. Create 2 QuickUnion items.
+    1 for percolates() and has a virtual bottom node, 2 for isFull() and does not have bottom.
+     */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("Number must be greater than 0!");
+        }
+        pLength = N;
+        pTron = new boolean[N][N];
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                pTron[i][j] = false;
+            }
+        }
+        openNodes = 0;
+        pList1 = new WeightedQuickUnionUF(N * N + 2);
+        pList2 = new WeightedQuickUnionUF(N * N + 1);
+        pTop = N * N;
+        pBottom = N * N + 1;
+        for (int i = 0; i < pLength; i += 1) {
+            pList1.union(pTop, i);
+            pList2.union(pTop, i);
+            pList1.union(pBottom, N * N - i - 1);
+        }
+    }
+
+    /* Check if percolates. */
+    public boolean percolates() {
+        return pList1.connected(pTop, pBottom);
+    }
+
+    /* Check the number of open nodes on the grid. */
+    public int numberOfOpenSites() {
+        return openNodes;
+    }
+
+    /*Check if the position is full or empty. */
+    public boolean isFull(int row, int col) {
+        if (!nodeValidate(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        return pTron[row][col] && pList2.connected(pTop, xyToNum(row, col));
+    }
+
+    /* Set position row, col on the grid to open. Connect to adjacent node if its open. */
+    public void open(int row, int col) {
+        if (!nodeValidate(row, col)) {
+            throw new IndexOutOfBoundsException();
+        } else if (pTron[row][col]) {
+            return;
+        }
+        pTron[row][col] = true;
+        openNodes += 1;
+        int pNode = xyToNum(row, col);
+        nodeConnect(pNode, row - 1, col);
+        nodeConnect(pNode, row + 1, col);
+        nodeConnect(pNode, row, col - 1);
+        nodeConnect(pNode, row, col + 1);
+    }
+
+    /* Check if the position is open or closed. */
+    public boolean isOpen(int row, int col) {
+        if (!nodeValidate(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        return pTron[row][col];
+    }
+
+    /* Connect if the node exist on the grid. */
+    private void nodeConnect(int node, int row, int col) {
+        if ( nodeValidate(row, col) && pTron[row][col]) {
+            pList1.union(node, xyToNum(row, col));
+            pList2.union(node, xyToNum(row, col));
+        }
+    }
+
+    /* Validate if integer is within bound. */
+    private boolean nodeValidate(int row, int col) {
+        return row >= 0 && row < pLength && col >= 0 && col < pLength;
+    }
+
+    /* Convert xy coordination to a number, and vice versa. */
+    private int xyToNum(int x, int y) {
+        return x * pLength + y;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,63 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+import java.lang.Math;
+
 public class PercolationStats {
 
+    private int sLength;
+    private double sMean;
+    private double stdDev;
+    private double confLow;
+    private double confHigh;
+
+    /* Generate a PercolationStats object with T numbers of experiments. */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Grid axis and number of experiments must be greater than zero!");
+        }
+        sLength = N;
+        // Perform a series of random experiments.
+        double[] sResults = new double[T];
+        for (int i = 0; i < T; i += 1) {
+            sResults[i] = randomExp(pf);
+        }
+        // Calculate desired results.
+        sMean = StdStats.mean(sResults);
+        stdDev = StdStats.stddev(sResults);
+        confLow = sMean - (1.96 * stdDev / (Math.sqrt(T)));
+        confHigh = sMean + (1.96 * stdDev / (Math.sqrt(T)));
+    }
+
+    /* Perform a random experiment and return ratio of open sites to total sites when percolation occur. */
+    private int randomExp(PercolationFactory pf) {
+        Percolation pRandom = pf.make(sLength);
+        while (!pRandom.percolates()) {
+            openRandom(pRandom);
+        }
+        return pRandom.numberOfOpenSites() / ( sLength * sLength );
+    }
+
+    /* Open a random node in Percolation object. */
+    private void openRandom(Percolation P) {
+        P.open(StdRandom.uniform(sLength), StdRandom.uniform(sLength));
+    }
+
+    public double mean() {
+        return sMean;
+    }
+
+    public double stddev() {
+        return stdDev;
+    }
+
+    public double confidenceLow() {
+        return confLow;
+    }
+
+    public double confidenceHigh() {
+        return confHigh;
+    }
 }
