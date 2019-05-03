a171ece883404f428cdf56dd8e420a06ec21d1f8
Yufan Ren <yufanren@yahoo.com>
2019-05-03 14:33:17 -0400

    Bear Maps Rasterer 1st Try

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.LinkedList;
 
 /**
  * This class provides all code necessary to take a query box and produce
@@ -9,8 +10,12 @@ import java.util.Map;
  */
 public class Rasterer {
 
+    private int uLLon;
+    private int uLLat;
+    private int lRLon;
+    private int lRLat;
+
     public Rasterer() {
-        // YOUR CODE HERE
     }
 
     /**
@@ -42,11 +47,118 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+        double[] lonDPPs = getDPPperLevel(new double[8]);
+        Integer depth = getLevel(params.get("lrlon"),
+                params.get("ullon"), params.get("w"), lonDPPs);
+        //Calculate depth of image.
+        results.put("depth", depth);
+        double l = Math.pow(2, depth);
+        long length = (long) l;
+        String[][] grid = getFiles(depth, length, params);
+        if (grid[0].length == 0) {
+            results.put("query_success", false);
+        } else {
+            results.put("query_success", true);
+        }
+        results.put("render_grid", grid);
+        results.put("raster_ul_lon", calAxis(uLLon, length, MapServer.ROOT_ULLON,
+                MapServer.ROOT_LRLON));
+        results.put("raster_lr_lon", calAxis(lRLon, length, MapServer.ROOT_ULLON,
+                MapServer.ROOT_LRLON));
+        results.put("raster_ul_lat", calAxis(uLLat, length, MapServer.ROOT_ULLAT,
+                MapServer.ROOT_LRLAT));
+        results.put("raster_lr_lat", calAxis(lRLat, length, MapServer.ROOT_ULLAT,
+                MapServer.ROOT_LRLAT));
         return results;
     }
 
+    /* Calculate Lon/Lat for a given point on the map, for theraster. */
+    private double calAxis(int a, long length, double lbound, double hbound) {
+        return lbound + a * (hbound - lbound) / length;
+    }
+
+    /* Find the files to display at specified depth. */
+    private String[][] getFiles(long depth, long length, Map<String, Double> params) {
+
+        Integer[] xFiles = getCoordinates(params.get("ullon"),
+                params.get("lrlon"), length, MapServer.ROOT_ULLON, MapServer.ROOT_LRLON);
+        // reversed input of low vs high.
+        Integer[] yFiles = getCoordinates(params.get("ullat"),
+                params.get("lrlat"), length, MapServer.ROOT_ULLAT, MapServer.ROOT_LRLAT);
+        uLLon = xFiles[0];
+        lRLon = xFiles[1] + 1;
+        uLLat = yFiles[0];
+        lRLat = yFiles[1] + 1;
+        LinkedList<Integer> xList = boundToList(xFiles, length);
+        LinkedList<Integer> yList = boundToList(yFiles, length);
+        String[][] grid = listToGrid(xList, yList, depth);
+        return grid;
+    }
+
+    /* Convert list of X and Y numbers to the files for display. */
+    private String[][] listToGrid(LinkedList<Integer> xlist, LinkedList<Integer> ylist,
+                                  long depth) {
+        String[][] grid = new String[ylist.size()][xlist.size()];
+        for (int i = 0; i < ylist.size(); i += 1) {
+            for (int j = 0; j < xlist.size(); j += 1) {
+                grid[i][j] = fNameConstructor(ylist.get(i), xlist.get(j), depth);
+            }
+        }
+        return grid;
+    }
+
+    /* create file name based on depth and grid position. */
+    private String fNameConstructor(int y, int x, long depth) {
+        String fileName = "d" + String.valueOf(depth) + "_x"
+                + String.valueOf(x) + "_y" + String.valueOf(y) + ".png";
+        return fileName;
+    }
+
+    /* Convert bounds to a list of valid numbers for locating files. */
+    private LinkedList<Integer> boundToList(Integer[] bounds, long length) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = bounds[0]; i <= bounds[1]; i += 1) {
+            if (i >= 0 && i <= length) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    /*Find the x coordinate of files to display. */
+    private Integer[] getCoordinates(double low, double high, long length,
+                                               double lBound, double hBound) {
+        Integer[] list = new Integer[2];
+        list[0] = getBound(low - lBound, (hBound - lBound) / length);
+        list[1] = getBound(high - lBound, (hBound - lBound) / length);
+        return list;
+    }
+
+    private int getBound(double bound, double lengthPerFile) {
+        double countDouble = bound / lengthPerFile;
+        int countInt = (int) countDouble;
+
+        return countInt;
+    }
+
+    /* Find the appropriate image quality to display. Return the greatest LonDPP that is less than
+    *   or equal to the LonDPP of the query, at maximun depth 7. */
+    private Integer getLevel(double lrlon, double ullon, double width, double[] lonDPPs) {
+        double lonDPP = (lrlon - ullon) / width;
+        for (int i = 0; i < 8; i += 1) {
+            if (lonDPPs[i] <= lonDPP) {
+                return i;
+            }
+        }
+        return 7;
+    }
+
+    /* Calculate the LonDPP for each level of zoom. */
+    private double[] getDPPperLevel(double[] dPPs) {
+        for (int i = 0; i < 8; i += 1) {
+            dPPs[i] = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / (256 * Math.pow(2, i));
+        }
+        return dPPs;
+    }
 }
